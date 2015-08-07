b00da738f629053261c448d7695a59fc8bc24570
Adam Fluke <fluke.adam@gmail.com>
2015-08-07 11:24:47 -0700

    Implement React Setup
    ^- I modified the webpack config file to remove the conflicting
    loaders.
    ^- I deleted the greeting.js since it was no longer neccessary.
    ^- The app.js now has the app component which renders the body
    from the components. This component should not change much, but
    is a useful place to store application wide state. Also call was
    added for us to use with ajax.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,42 @@
 import React from "react";
-import Greeting from "./greeting";
+import Body from "./components/Body";
 
+//functions defined in the global scope to be used in many components
+var call = function(action, method, data){
+  return new Promise(function(resolve, reject){
+    request = $.ajax({
+      url:      action,
+      method:   method,
+      data:     data,
+      dataType: "json"
+    });
+
+    request.done(function(serverData){
+      resolve(serverData)
+    });
+
+    request.fail(function(serverData){
+      reject(serverData)
+    });
+  });
+}
+
+//Top Level app component that manages whole app state
+var App = React.createClass({
+  getInitialState: function(){
+    return {
+      page: 'home',
+      user: null,
+    }
+  },
+  render: function(){
+    return (
+      <div id="main">
+        <Body />
+      </div>
+    )
+  }
+});
+
+
+React.render(<App />, document.body);
