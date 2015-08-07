6eec840b1e565dacc68e5ec6212a0cfcd161bca1
Adam Fluke <fluke.adam@gmail.com>
2015-08-07 11:29:17 -0700

    Create components folder and body/footer components.

---
+++ app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default React.createClass({
+  render: function() {
+    return (
+      <div id="footer" className="bg-g h100 tac pt30px cr">
+        <p>&copy; SmartText</p>
+      </div>
+    );
+  },
+});
