c9153299500c333190e5f46b84cc0d84a41e12db
Aaron Jacobowitz <amjacobowitz@gmail.com>
2015-08-06 20:43:20 -0700

    add webpack and gitignore

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  context: __dirname + "/app",
+  entry: {
+    javascript: "./app.js",
+    html: "./index.html",
+  },
+  output: {
+    filename: "app.js",
+    path: __dirname + "/distribution",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"],
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["react-hot", "babel-loader"],
+      }
+    ]
+  }
+};
+
