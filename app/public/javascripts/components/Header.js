161432e789d97749ed68b7d0076fc1780e0c27f3
cjmanetta <cjmanetta@gmail.com>
2015-08-08 12:31:49 -0700

    start header component

--- app/public/javascripts/components/Header.js
+++ app/public/javascripts/components/Header.js
@@ -1,11 +1,36 @@
 var React = require("react");
 
 var Header = React.createClass({
+
   render: function(){
+    var teacher = this.props.user.teacher
+    var student = this.props.user.student
+    var content = null
+
+    if (teacher) {
+      content = <p className="navbar-text">{teacher.first_name} {teacher.last_name}</p>
+      }
+    } else if (student) {
+      content = <p className="navbar-text">{user.first_name}</p>
+      }
+    } else
+      content = null
+    }
+    //
+    //if props.user.teacher is not null make this content
+    //else
+    // if props.user.student is not null make this content
+    //else
+    //generic content
     return (
-      <div>header</div>
+      //add full navbar components brand buttons etc
+      <div>{content}</div>
     )
   }
 });
 
 module.exports = Header;
+
+// var scores = this.props.scores.map(function(score) {
+//       return <Score key={score.id} score={score} />
+//     });
--- app/public/javascripts/components/Home.js
+++ app/public/javascripts/components/Home.js
@@ -2,16 +2,17 @@ var React = require("react");
 //below this import follow this syntax to add
 //a new component. Save it in this file with capital
 //file names to show that it is a react file
-var Footer = require("./Footer");
+var Header = require("./Header");
 var SignUp = require("./SignUp");
 
 
 var Body = React.createClass({
   render: function() {
+    var teacher = {_id: "22", first_name: "sally", last_name: "bates", username: "sbates", password: "1234"}
     return (
       <div id="main" className="container">
+        <Header teacher={teacher}/>
         <SignUp />
-        <Footer />
       </div>
     );
   },
