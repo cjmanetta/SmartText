c18ae51069784b3cfbb717563f056fc026708587
Aaron Jacobowitz <amjacobowitz@gmail.com>
2015-08-09 12:07:40 -0700

    add populated nested class and students, but not yet actually saving students in the student route

--- app/routes/teacher.js
+++ app/routes/teacher.js
@@ -117,41 +117,31 @@ router.route('/:id')
   Teacher.findOne({_id: req.params.id})
   	.populate('klasses')
   	.exec(function(err, teacher){
-	    if (err){
-	      return console.error(err);
-	    } else {
-	    	console.log(teacher)
-	      res.format({
-	        'text/html': function(){
-	          res.render('./teachers/show', { teacher: teacher})
-	        },
-	        'application/json': function(){
-	          res.send({teacher: teacher})
-	        }
-	      })
-    	}
-  })
+  		for (var i=0; i<teacher.klasses.length; i++){
+  			Klass.findOne({_id: teacher.klasses[i]._id})
+  				.populate('students')
+  				.exec(function(err, klass){
+				    if (err){
+				      return console.error(err);
+				    } else {
+				    	console.log(klass)
+				    }
+				 	})
+  		}
+  	})
+  Teacher.findOne({_id: req.params.id}, function(err, teacher){
+  	console.log(teacher)
+		res.format({
+		  'text/html': function(){
+		    res.render('./teachers/show', { teacher: teacher})
+		  },
+		  'application/json': function(){
+		    res.send({teacher: teacher})
+		  }
+		})	
+  })	   
 })
 
-
-
-
-// Teacher.findById(req.params.id, function(err, teacher){
-// 	if (err){
-// 		return console.error(err);
-// 	} else {
-// 		console.log(req.params.id)
-// 		console.log(teacher)
-// 		res.format({
-// 			'text/html': function(){
-// 				res.render('./teachers/show', {teacher: teacher})
-// 			},
-// 			'application/json': function(){
-// 				res.send({teacher: teacher})
-// 			}
-// 		})
-// 	}
-// })
 
 .put(function(req, res){
 	Teacher.findById(req.params.id, function(err, teacher){
