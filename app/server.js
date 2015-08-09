8d81f3d0bc4c85a59c22b1b09dd8fa134f9657d1
Aaron Jacobowitz <amjacobowitz@gmail.com>
2015-08-08 18:00:48 -0700

    add procfile

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var port = 8080;
+var port = process.env.PORT || 5000;
 
 
 server.listen(port)
