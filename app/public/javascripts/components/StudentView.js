bd1b42a7323876ffc7cb2683e839913368ef1115
Aaron Jacobowitz <amjacobowitz@gmail.com>
2015-08-11 14:11:31 -0700

    selection working on teacher side

--- app/public/javascripts/components/StudentView.js
+++ app/public/javascripts/components/StudentView.js
@@ -14,7 +14,7 @@ var StudentView = React.createClass({
       article: {content: "Lars Brandsson was up on the ladder, on the tall and abrupt roof of the house, with a couple of nails between his lips, knockingwith hammer in hand. The sun, gleaming in white hue, had justslid above the distant mountain ridges in the East. A robinshrilled hidden in some trees nearby, its chirping covered by theinterrupted pounding of the hammer. Trampling of hooves soundedfrom the road and a young man of about seventeen approached onhorse, dressed in thin linen shirt opened at the chest, with an axe girded at the waist and fishing utensils arrayed on the saddle. It was Helgi Dagsson. Lars Brandsson glanced to the sidea moment, wiping some loose strands of hair off his face andarranging them behind his ears, then went on to hammer the nailinto the wood.", author: "Charlotte Manetta", title: "The Amazing Zamboni"},
       highlightOn: false,
       activeLesson: {},
-      question: {prompt: "How are you?", green_start: 241, green_end: 284},
+      question: {prompt: "", green_start: null, green_end: null},
       // lesson: new Lession(),
       selections: [],
     }
@@ -35,14 +35,14 @@ var StudentView = React.createClass({
         highlightOn: false
       });
     })
-    socket.emit('addStudent', {user: this.state.user})
+    socket.emit('addStudent', {student: this.state.student})
   },
   // componentWillUnmount: function() {
   //   this.state.lesson.off('change', this.lessonChanged)
   // },
   updatePrompt: function(data){
     this.setState({
-      // question.prompt: data,
+      question: data,
       highlightOn: true
     })
   },
@@ -58,80 +58,67 @@ var StudentView = React.createClass({
   },
   handleSelect: function(selection){
     // var socket = io('/teacher')
-    // if (this.state.highlightOn){
+    if (this.state.highlightOn){
       // var correctColor = this.compareSelection(selection);
+      var correctColor = 'blue'
       var selectedRange = selection.getRangeAt(0);
       this.state.selections.push(selectedRange);
       this.forceUpdate();
-      // var selectedText = selectedRange.extractContents()
 
-      // var highlightSpan = $("<span class='highlight'>" +
-      //                     selectedText.textContent + "</span>");
+      var highlightedText = $('#content').html()
 
-      // selectedRange.insertNode(highlightSpan[0]);
-
-      // var highlightedText = $('#content').html()
-
-      //can remove the console.log once it is tested over
-      //the socket
-      // console.log({
-      //   user: this.state.user,
-      //   selection: highlightedText,
-      //   color: correctColor
-      // });
-
-      // socket.emit('select', {
-      //   user: this.state.user,
-      //   selection: highlightedText,
-      //   color: correctColor,
-      //   id: this.state.user.id
-      // })
-    // }
+      socket.emit('select', {
+        student: this.state.student,
+        selection: highlightedText,
+        color: correctColor,
+        id: this.state.student.id
+      })
+    }
   },
-  compareSelection: function(selection){
-    var student_start = selection.anchorOffset;
-    var student_end = selection.focusOffset;
-    var correct_start = this.state.lesson.correct.start;
-    var correct_end = this.state.lesson.correct.end;
+  // compareSelection: function(selection){
+  //   var student_start = selection.anchorOffset;
+  //   var student_end = selection.focusOffset;
+  //   var correct_start = this.state.lesson.correct.start;
+  //   var correct_end = this.state.lesson.correct.end;
 
-    //adjust start/end regardless of which way they highlight
-    if(student_start > student_end){
-      student_start = selection.focusOffset;
-      student_end = selection.anchorOffset;
-    }
-    if(correct_start > correct_end){
-      correct_start = this.state.lesson.correct.end;
-      correct_end = this.state.lesson.correct.start;
-    }
+  //   //adjust start/end regardless of which way they highlight
+  //   if(student_start > student_end){
+  //     student_start = selection.focusOffset;
+  //     student_end = selection.anchorOffset;
+  //   }
+  //   if(correct_start > correct_end){
+  //     correct_start = this.state.lesson.correct.end;
+  //     correct_end = this.state.lesson.correct.start;
+  //   }
 
-    var correct_length = correct_end - correct_start;
-    var variance = Math.round(correct_length / 6);
-    var correct_start_range_beginning = correct_start - variance;
-    var correct_start_range_end = correct_start + variance;
-    var correct_end_range_beginning = correct_end - variance;
-    var correct_end_range_end = correct_end + variance;
+  //   var correct_length = correct_end - correct_start;
+  //   var variance = Math.round(correct_length / 6);
+  //   var correct_start_range_beginning = correct_start - variance;
+  //   var correct_start_range_end = correct_start + variance;
+  //   var correct_end_range_beginning = correct_end - variance;
+  //   var correct_end_range_end = correct_end + variance;
 
 
-    if(student_start > correct_start_range_beginning && student_start < correct_start_range_end){
-      if(student_end > correct_end_range_beginning && student_end < correct_end_range_end){
-        var color = '#76EE00'
-      } else {
-        var color = 'blue'
-      }
-    } else if(student_end > correct_end_range_beginning && student_end < correct_end_range_end){
-      var color = 'blue'
-    } else if(student_start > correct_start && student_start < correct_end){
-      var color = 'blue'
-    } else if(student_end > correct_start && student_end < correct_end){
-      var color = 'blue'
-    } else if(student_start < correct_start && student_end > correct_end){
-      var color = 'blue'
-    } else {
-      var color = 'red'
-    }
+  //   if(student_start > correct_start_range_beginning && student_start < correct_start_range_end){
+  //     if(student_end > correct_end_range_beginning && student_end < correct_end_range_end){
+  //       var color = '#76EE00'
+  //     } else {
+  //       var color = 'blue'
+  //     }
+  //   } else if(student_end > correct_end_range_beginning && student_end < correct_end_range_end){
+  //     var color = 'blue'
+  //   } else if(student_start > correct_start && student_start < correct_end){
+  //     var color = 'blue'
+  //   } else if(student_end > correct_start && student_end < correct_end){
+  //     var color = 'blue'
+  //   } else if(student_start < correct_start && student_end > correct_end){
+  //     var color = 'blue'
+  //   } else {
+  //     var color = 'red'
+  //   }
 
-    return color;
-  },
+  //   return color;
+  // },
   render: function() {
     return (
       <div className="container">
--- app/public/javascripts/distribution/bundle.js
+++ app/public/javascripts/distribution/bundle.js
@@ -25177,7 +25177,7 @@
 	      article: { content: "Lars Brandsson was up on the ladder, on the tall and abrupt roof of the house, with a couple of nails between his lips, knockingwith hammer in hand. The sun, gleaming in white hue, had justslid above the distant mountain ridges in the East. A robinshrilled hidden in some trees nearby, its chirping covered by theinterrupted pounding of the hammer. Trampling of hooves soundedfrom the road and a young man of about seventeen approached onhorse, dressed in thin linen shirt opened at the chest, with an axe girded at the waist and fishing utensils arrayed on the saddle. It was Helgi Dagsson. Lars Brandsson glanced to the sidea moment, wiping some loose strands of hair off his face andarranging them behind his ears, then went on to hammer the nailinto the wood.", author: "Charlotte Manetta", title: "The Amazing Zamboni" },
 	      highlightOn: false,
 	      activeLesson: {},
-	      question: { prompt: "How are you?", green_start: 241, green_end: 284 },
+	      question: { prompt: "", green_start: null, green_end: null },
 	      // lesson: new Lession(),
 	      selections: []
 	    };
@@ -25198,14 +25198,14 @@
 	        highlightOn: false
 	      });
 	    });
-	    socket.emit('addStudent', { user: this.state.user });
+	    socket.emit('addStudent', { student: this.state.student });
 	  },
 	  // componentWillUnmount: function() {
 	  //   this.state.lesson.off('change', this.lessonChanged)
 	  // },
 	  updatePrompt: function updatePrompt(data) {
 	    this.setState({
-	      // question.prompt: data,
+	      question: data,
 	      highlightOn: true
 	    });
 	  },
@@ -25221,79 +25221,66 @@
 	  },
 	  handleSelect: function handleSelect(selection) {
 	    // var socket = io('/teacher')
-	    // if (this.state.highlightOn){
-	    // var correctColor = this.compareSelection(selection);
-	    var selectedRange = selection.getRangeAt(0);
-	    this.state.selections.push(selectedRange);
-	    this.forceUpdate();
-	    // var selectedText = selectedRange.extractContents()
+	    if (this.state.highlightOn) {
+	      // var correctColor = this.compareSelection(selection);
+	      var correctColor = 'blue';
+	      var selectedRange = selection.getRangeAt(0);
+	      this.state.selections.push(selectedRange);
+	      this.forceUpdate();
 
-	    // var highlightSpan = $("<span class='highlight'>" +
-	    //                     selectedText.textContent + "</span>");
+	      var highlightedText = $('#content').html();
 
-	    // selectedRange.insertNode(highlightSpan[0]);
-
-	    // var highlightedText = $('#content').html()
-
-	    //can remove the console.log once it is tested over
-	    //the socket
-	    // console.log({
-	    //   user: this.state.user,
-	    //   selection: highlightedText,
-	    //   color: correctColor
-	    // });
-
-	    // socket.emit('select', {
-	    //   user: this.state.user,
-	    //   selection: highlightedText,
-	    //   color: correctColor,
-	    //   id: this.state.user.id
-	    // })
-	    // }
+	      socket.emit('select', {
+	        student: this.state.student,
+	        selection: highlightedText,
+	        color: correctColor,
+	        id: this.state.student.id
+	      });
+	    }
 	  },
-	  compareSelection: function compareSelection(selection) {
-	    var student_start = selection.anchorOffset;
-	    var student_end = selection.focusOffset;
-	    var correct_start = this.state.lesson.correct.start;
-	    var correct_end = this.state.lesson.correct.end;
+	  // compareSelection: function(selection){
+	  //   var student_start = selection.anchorOffset;
+	  //   var student_end = selection.focusOffset;
+	  //   var correct_start = this.state.lesson.correct.start;
+	  //   var correct_end = this.state.lesson.correct.end;
 
-	    //adjust start/end regardless of which way they highlight
-	    if (student_start > student_end) {
-	      student_start = selection.focusOffset;
-	      student_end = selection.anchorOffset;
-	    }
-	    if (correct_start > correct_end) {
-	      correct_start = this.state.lesson.correct.end;
-	      correct_end = this.state.lesson.correct.start;
-	    }
+	  //   //adjust start/end regardless of which way they highlight
+	  //   if(student_start > student_end){
+	  //     student_start = selection.focusOffset;
+	  //     student_end = selection.anchorOffset;
+	  //   }
+	  //   if(correct_start > correct_end){
+	  //     correct_start = this.state.lesson.correct.end;
+	  //     correct_end = this.state.lesson.correct.start;
+	  //   }
 
-	    var correct_length = correct_end - correct_start;
-	    var variance = Math.round(correct_length / 6);
-	    var correct_start_range_beginning = correct_start - variance;
-	    var correct_start_range_end = correct_start + variance;
-	    var correct_end_range_beginning = correct_end - variance;
-	    var correct_end_range_end = correct_end + variance;
+	  //   var correct_length = correct_end - correct_start;
+	  //   var variance = Math.round(correct_length / 6);
+	  //   var correct_start_range_beginning = correct_start - variance;
+	  //   var correct_start_range_end = correct_start + variance;
+	  //   var correct_end_range_beginning = correct_end - variance;
+	  //   var correct_end_range_end = correct_end + variance;
 
-	    if (student_start > correct_start_range_beginning && student_start < correct_start_range_end) {
-	      if (student_end > correct_end_range_beginning && student_end < correct_end_range_end) {
-	        var color = '#76EE00';
-	      } else {
-	        var color = 'blue';
-	      }
-	    } else if (student_end > correct_end_range_beginning && student_end < correct_end_range_end) {
-	      var color = 'blue';
-	    } else if (student_start > correct_start && student_start < correct_end) {
-	      var color = 'blue';
-	    } else if (student_end > correct_start && student_end < correct_end) {
-	      var color = 'blue';
-	    } else if (student_start < correct_start && student_end > correct_end) {
-	      var color = 'blue';
-	    } else {
-	      var color = 'red';
-	    }
+	  //   if(student_start > correct_start_range_beginning && student_start < correct_start_range_end){
+	  //     if(student_end > correct_end_range_beginning && student_end < correct_end_range_end){
+	  //       var color = '#76EE00'
+	  //     } else {
+	  //       var color = 'blue'
+	  //     }
+	  //   } else if(student_end > correct_end_range_beginning && student_end < correct_end_range_end){
+	  //     var color = 'blue'
+	  //   } else if(student_start > correct_start && student_start < correct_end){
+	  //     var color = 'blue'
+	  //   } else if(student_end > correct_start && student_end < correct_end){
+	  //     var color = 'blue'
+	  //   } else if(student_start < correct_start && student_end > correct_end){
+	  //     var color = 'blue'
+	  //   } else {
+	  //     var color = 'red'
+	  //   }
 
-	    return color;
-	  },
+	  //   return color;
+	  // },
 	  render: function render() {
 	    return React.createElement(
 	      'div',
@@ -25457,7 +25444,6 @@
 	          this.getEnd(selections)
 	        )
 	      );
-	      debugger;
 	    }
 
 	    return React.createElement(
@@ -27004,15 +26990,13 @@
 	  getInitialState: function getInitialState() {
 	    return {
 	      article: { author: "Charlotte Manetta", title: "The Amazing Zamboni", content: "Lars Brandsson was up on the ladder, on the tall and abrupt roof of the house, with a couple of nails between his lips, knockingwith hammer in hand. The sun, gleaming in white hue, had justslid above the distant mountain ridges in the East. A robinshrilled hidden in some trees nearby, its chirping covered by theinterrupted pounding of the hammer. Trampling of hooves soundedfrom the road and a young man of about seventeen approached onhorse, dressed in thin linen shirt opened at the chest, with an axe girded at the waist and fishing utensils arrayed on the saddle. It was Helgi Dagsson. Lars Brandsson glanced to the sidea moment, wiping some loose strands of hair off his face andarranging them behind his ears, then went on to hammer the nailinto the wood." },
-	      user: { first_name: "TEACHER", last_name: "A", username: "hello", id: '123' },
-	      prompt: 'Please look at the text and highlight the best example of a character showing caring.',
+	      question: { prompt: "How are you?", green_start: 241, green_end: 284 },
 	      students: [],
 	      clickable: true,
 	      tileBig: false
 	    };
 	  },
 	  componentDidMount: function componentDidMount() {
-	    this.getLesson();
 	    var that = this;
 	    socket.on('select', function (data) {
 	      that.updateStudentTile(data);
@@ -27030,7 +27014,6 @@
 	    $('#' + data.id).find('div').css("border-color", 'black');
 	  },
 	  updateStudentTile: function updateStudentTile(data) {
-	    console.log('inside update');
 	    var textFromStudent = data.selection;
 	    var borderColor = data.color;
 	    $('#' + data.id).find('#content').html(textFromStudent);
@@ -27050,33 +27033,20 @@
 	  },
 	  addStudent: function addStudent(data) {
 	    var students = this.state.students;
-	    students.push(data.user);
+	    students.push(data.student);
 	    console.log('students array: ' + students);
 	    this.setState({
 	      students: students
 	    });
 	  },
 	  viewPrompt: function viewPrompt() {
-	    socket.emit('viewPrompt', this.state.prompt);
+	    socket.emit('viewPrompt', this.state.question);
 	  },
 	  disableStudents: function disableStudents() {
 	    socket.emit('finish');
 	  },
-	  getLesson: function getLesson() {
-	    //here is where the api call would happen
-	    //to recieve the lesson which is active
-	    //for that class
-
-	    //stubbed for right now
-	    var newLesson = {};
-
-	    this.setState({
-	      lesson: newLesson
-	    });
-	  },
 	  render: function render() {
 
-	    var lesson = this.state.lesson;
 	    var that = this;
 	    var students = this.state.students.map(function (student) {
 	      return React.createElement(
@@ -27100,7 +27070,7 @@
 	      ),
 	      React.createElement(RouteHandler, null),
 	      students,
-	      React.createElement(RightBar, { prompt: this.state.prompt, actionOne: this.viewPrompt, actionTwo: this.disableStudents, labelOne: "view question", labelTwo: "finished" })
+	      React.createElement(RightBar, { question: this.state.question, actionOne: this.viewPrompt, actionTwo: this.disableStudents, labelOne: "view question", labelTwo: "finished" })
 	    );
 	  }
 	});
